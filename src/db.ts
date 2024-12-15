b989390470c5b736f38d93d666450d80b94d22d9
Mayank Balmoori <balmoorimayank@gmail.com>
2024-12-15 16:09:39 +0530

    Added the sign-up and sign-in endpoints

--- src/db.ts
+++ src/db.ts
@@ -6,23 +6,8 @@ const objectId = Schema.ObjectId;
 //Declaring Schemas
 
 const userSchema = new Schema({
-    username: { type: String, minLength: [3, "Must be at least 3 characters"], unique: true, required: true },
-    password: {
-        type: String,
-        validate: {
-            validator: function isValidPassword(password: string): boolean {
-                const hasUpperCase = /[A-Z]/.test(password);
-                const hasLowerCase = /[a-z]/.test(password);
-                const hasDigit = /\d/.test(password);
-                const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
-                const hasMinLength = password.length >= 8;
-
-                return hasUpperCase && hasLowerCase && hasDigit && hasSpecialChar && hasMinLength;
-            },
-            message: `Password must be at least 8 characters long and include an uppercase letter, lowercase letter, number, and special character.`
-        }
-        , required: [true, "Please enter username"]
-    }
+    username: { type: String, unique: true, required: true },
+    password: { type: String, required: [true, "Please enter username"] }
 })
 
 const contentSchema = new Schema(+{
--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,118 @@
 import express from "express"
 import jwt from "jsonwebtoken"
 import dotenv from "dotenv"
+import { string, z } from 'zod'
+import bcrypt from 'bcrypt'
+import { Users, Content, Tags, Links } from './db'
+import mongoose from "mongoose"
 
 const app = express();
 dotenv.config();
+async function dbConnect() {
+    await mongoose.connect(process.env.DB_CONNECTION_STRING + "");
+}
+dbConnect();
 
 app.use(express.json());
-app.get("/",function (req,res){
+app.get("/", function (req, res) {
     res.json({
         msg: "You've reached here successfully"
     })
 })
-app.post("/api/v1/signup",function (req,res){
-   
+app.post("/api/v1/signup", async function (req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const inputValidation = z.object({
+        username: z.string().min(3, { message: "Must be at least 3 characters" }),
+        password: z.string().regex(new RegExp(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).{8,}$/), "Password must be at least 8 characters long and include an uppercase letter, lowercase letter, number, and special character.")
+    })
+    const validationIsSuccess = inputValidation.safeParse(req.body);
+    if (validationIsSuccess) {
+        const hashedPassword = bcrypt.hashSync(password, 5);
+
+        try {
+            const isUser = await Users.findOne({
+                username: username
+            })
+            if (isUser) {
+                res.status(403).json({
+                    msg: "User already exists"
+                })
+            }
+            await Users.create({
+                username: username,
+                password: hashedPassword
+            })
+            res.status(200).json({
+                msg: "You've been Signed Up"
+            })
+        } catch (error) {
+            res.status(500).json({
+                msg: error
+            })
+        }
+    }
+    else {
+        res.status(411).json({
+            msg: "Error in inputs"
+        })
+    }
 })
-app.post("/api/v1/signin",function (req,res){
+app.post("/api/v1/signin", async function (req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const isValid = z.object({
+        username: z.string().min(3, "Must be at least 3 characters"),
+        password: z.string()
+    })
+
+    const validatedSuccessfully = isValid.safeParse(req.body);
+    let user;
+    if (validatedSuccessfully) {
+        try {
+            user = await Users.findOne({
+                username: username
+            })
+        } catch (error) {
+            res.status(503).json({
+                msg: "Server side error"
+            })
+        }
+        if (user) {
+            const verifiedPassword = bcrypt.compareSync(password, user.password);
+            if (verifiedPassword) {
+                const token = jwt.sign(user._id.toString(), process.env.JWT_SECRET + "");
+                res.status(200).json({
+                    token: token
+                })
+            }
+            else{
+                res.status(403).json({
+                    msg : "Wrong Password"
+                })
+            }
+        }
+        else{
+            res.status(404).json({
+                msg: "No such user exists"
+            })
+        }
+    }
+    else{
+        res.status(400).json({
+            msg: "Incorrect input format"
+        })
+    }
+})
+app.post("/api/v1/content", function (req, res) {
 
 })
-app.post("/api/v1/content",function (req,res){
+app.get("/api/v1/content", function (req, res) {
 
 })
-app.get("/api/v1/content",function(req,res){
-
-})
-app.delete("/api/v1/content",function (req,res){
+app.delete("/api/v1/content", function (req, res) {
 
 })
 app.listen(3000)
